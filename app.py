35779731f43e1f478a6ed7ad021b5a0520258a80
Aditya singh <adityasingh@Adityas-MacBook-Air.local>
2025-12-01 16:44:02 +0530

    Model Trainer

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from src.MLproject.components.data_ingestion import DataIngestion
 from src.MLproject.components.data_ingestion import DataIngestionConfig
 from src.MLproject.components.data_transformation import DataTransformationConfig,DataTransformation
 import sys
+from src.MLproject.components. model_trainer import ModelTrainerConfig, ModelTrainer
+
 
 if __name__ == "__main__":
     logging.info("The execution has started")
@@ -16,7 +18,12 @@ if __name__ == "__main__":
 
         #DataTransformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+         ##Model training
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr, test_arr)
+)
 
     
     except Exception as e:
--- src/MLproject/utils.py
+++ src/MLproject/utils.py
@@ -5,7 +5,8 @@ from src.MLproject.logger import logging
 import pandas as pd
 from dotenv import load_dotenv
 import pymysql
-
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 import pickle 
 import numpy as np
 
@@ -37,15 +38,51 @@ def read_sql_data():
 
     except Exception as ex:
         raise CustomException(ex, sys)
-    
+
+
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
         os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path,"wb") as file_obj:
+        with open(file_path, "wb") as file_obj:
             pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e,sys)    
+        raise CustomException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    try:
+        report = {}
+
+        for model_name in models:
+            model = models[model_name]
+            param = params.get(model_name, {})
+
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            test_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+
+def load_objects(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
